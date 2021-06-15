dafa0c4da2755564128deaa4cd7eab6b16ee98f2
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-06-15 12:38:47 +0545

    demo mode

--- store/providers/NewsProvider.js
+++ store/providers/NewsProvider.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NewsContext from '../contexts/NewsContext';
-
+import axios from 'axios';
+const BASE_URL = "https://newsapi.org/v2";
+const API_KEY = "0bbe885d29d8460d8ac4ece98af81d64";
 class NewsProvider extends React.Component {
     state = {
         allNews: [],
@@ -9,16 +11,43 @@ class NewsProvider extends React.Component {
         newsDetail: {}
     }
 
-    getNewsFromAPI = () => {
+    getNewsFromAPI = async() => {
+        try{
+            const response = await axios.get(`${BASE_URL}/everything?q=bitcoin&apiKey=${API_KEY}`);
+            this.setState({
+                ...this.state,
+                allNews: response.data.articles
+            })
 
+        }
+        catch(error) {
+            console.log(error )
+        }
     }
 
-    getSourcesFromAPI = () => {
-
+    getSourcesFromAPI = async() => {
+        try{
+            const sourApi = await axios.get(`${BASE_URL}/sources?apiKey=${API_KEY}`);
+            this.setState({
+                ...this.state,
+                sources: sourApi.data.sources
+            })
+        }
+        catch(err){
+            console.log('sources', err)
+        }
     }
 
-    getHeadLinesFromAPI = () => {
-
+    getHeadLinesFromAPI = async() => {
+        try{
+            const headNews= await axios.get(`${BASE_URL}/top-headlines?country=us&apiKey=${API_KEY}`);
+            this.setState({
+                ...this.state,
+                headlines: headNews.data.articles
+            })
+        }
+        catch(error) {
+        }
     }
 
     setNewsDetail = (news) => {
