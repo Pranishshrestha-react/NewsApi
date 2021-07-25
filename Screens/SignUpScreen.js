a1165bfdce9537fbf8a2a6189df7dbc8daa1f8c0
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-07-25 13:08:33 +0545

    signup created

---
+++ Screens/SignUpScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const SignUpScreen= props =>{
+    return <View>
+            <Text>This is the SignupScreen. Please fill the credentials</Text>
+    </View>
+
+}
+ 
+const styles = StyleSheet.create({
+
+});
+
+export default SignUpScreen;
