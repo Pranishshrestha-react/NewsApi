874362f519f7ad00d765339d2f77d2ab319cfe6a
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-07-25 12:58:13 +0545

    login page created

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const LoginScreen= props =>{
+    return <View>
+            <Text>This is the login screen.</Text>
+    </View>
+
+}
+ 
+const styles = StyleSheet.create({
+
+});
+
+export default LoginScreen;
